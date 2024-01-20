28d7d3fc408e0651c358ba3407d8e7f03273401f
Lukindo Mbuli <lukindo.mbuli@gmail.com>
2024-01-20 18:16:06 +0100

    add more control flow, seems there is a bug with the filtering of products in the side menu

--- reactive-patterns/src/app/product-view/detail-view/detail-view.component.ts
+++ reactive-patterns/src/app/product-view/detail-view/detail-view.component.ts
@@ -1,20 +1,16 @@
 import { Component, inject } from '@angular/core';
-import { ProductService } from '../../services/product.service';
 import { ProductSignalsService } from 'src/app/services/product-signals.service';
 
 @Component({
   selector: 'app-detail-view',
   templateUrl: './detail-view.component.html',
-  styleUrls: ['./detail-view.component.css']
+  styleUrls: ['./detail-view.component.css'],
 })
 export class DetailViewComponent {
-  // The inject function is the same as declaring the service inside the constructor
-  private readonly productService = inject(ProductService);
+
   // TODO: signals service comment out productService and uncomment the next line
-  // private readonly productsSignals = inject(ProductSignalsService)
+  private readonly productsSignals = inject(ProductSignalsService)
 
   // We are using declarative style programming to set this property equal to the selectedProduct observable
-  readonly selectedProduct$ = this.productService.selectedProduct$;
-  // TODO: signals service comment out productService and uncomment the next line
-  // selectedProduct = this.productsSignals.selectedProduct;
+  selectedProduct = this.productsSignals.selectedProduct;
 }
